0270c67261d8148f10e4ed8ddb54912abbb7312d
= <=>
2022-12-10 22:02:22 -0700

    Varias rutas con express

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const port = 19132
 
 app.get("/", (req, res)=>{
     res.send("Hello world")
+});
+
+app.get("/nueva-ruta", (req, res) =>{
+    res.send("Hola, soy una nueva ruta")
+})
+
+app.get("/products", (req, res) =>{
+    res.json({
+        name: "Celular",
+        price: "7000"
+    })
 })
 
 app.listen(port, () => {
