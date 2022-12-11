cb628ea409e1c5239368a963a83742c2b1ebad07
= <=>
2022-12-10 21:56:54 -0700

    Primer pagina con express

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const express = require("express")
 const app = express();
 const port = 19132
 
+app.get("/", (req, res)=>{
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log("Servidor corriendo en: " + port)
+})
