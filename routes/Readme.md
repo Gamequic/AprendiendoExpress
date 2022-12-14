b8f3746cf717f4ce834c04a27d3c94afcfb23cd4
= <=>
2022-12-14 00:17:01 -0700

    Comenzar a usar Status Codes

---
+++ routes/Readme.md
@@ -0,0 +1,10 @@
+HTTP reponse status codes
+
+Informational responses (100-199)
+Successful responses (200-299)
+Redirects (300-399)
+Client errors (400-499)
+Server errors (500-599)
+
+https.cat
+Es una pagina que muestra el significado de diferentes codigos de HTTP
--- routes/products.js
+++ routes/products.js
@@ -26,18 +26,24 @@ router.get('/filter', (req, res) => {
 router.get("/:id", (req, res)=>{
     //const id = req.paramsa.id;
     const {id} = req.params
-    res.json({
-        id,
-        name: "Computadora",
-        price: "20000"
-    })
+    if (id === "999") {
+        res.status(404).json({
+            message: "not found"
+        })
+    } else {
+        res.status(200).json({
+            id,
+            name: "Computadora",
+            price: "20000"
+        })
+    }
 })
 
 
 
 router.post("/", (req, res) => {    //Recibir peticiones por post
     const body = req.body;
-    res.json({
+    res.status(201).json({
         message: "created",
         data: body
     })
