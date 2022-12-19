3a63f2d007b459c24bb4efaa8511e9da241647df
= <=>
2022-12-19 00:15:18 -0700

    MiddleWares2

--- routes/products.js
+++ routes/products.js
@@ -16,15 +16,10 @@ router.get('/filter', (req, res) => {
     res.send("Yo soy un filter")
 })
 
-router.get("/:id", async (req, res, next)=>{
-    try {
-      //const id = req.paramsa.id;
-      const { id } = req.params;
-      const product = service.findOne(id);
-      res.json(product);
-    } catch (error) {
-      next(error);
-    }
+router.get("/:id", async (req, res)=>{
+    const { id } = req.params
+    const product = service.findOne(id)
+    res.json(product)
 })
 
 
